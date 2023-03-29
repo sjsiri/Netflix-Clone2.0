0212a62ba463bceca2410fccc02d58f343aa355e
Surinh Siri <surinhjs@gmail.com>
2023-03-29 14:58:42 -0500

    added username, email, password

--- netflix-clone/components/Input.tsx
+++ netflix-clone/components/Input.tsx
@@ -1,8 +1,27 @@
-const Input = () => {
+import React from 'react';
+
+interface InputProps {
+    id: string;
+    onChange: any;
+    value: string;
+    label: string;
+    type?: string;
+}
+
+const Input: React.FC<InputProps> = ({
+    id,
+    onChange,
+    value,
+    label,
+    type
+}) => {
     return (
         <div className="relative">
         <input 
-            id="email"
+            onChange={onChange}
+            type={type}
+            value={value}
+            id={id}
             className="
                 block
                 rounded-md
@@ -38,8 +57,8 @@ const Input = () => {
             peer-focus:scale-75
             peer-focus:-translate-y-3
         " 
-        htmlFor="email">
-            Email
+        htmlFor={id}>
+            {label}
             </label>
         </div>
     )
